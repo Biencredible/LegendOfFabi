9f3f1d605b092e1bf7559a645decdb3790579d5c
Biencredible <fabian.bien@gmx.de>
2023-02-03 14:30:12 +0100

    keyboard input for player

--- code/level.py
+++ code/level.py
@@ -2,6 +2,7 @@ import pygame
 from settings import *
 from tile import Tile
 from player import Player
+from debug import debug
 
 class Level:
     def __init__(self):
@@ -23,7 +24,7 @@ class Level:
                 if column == 'x':
                     Tile((x, y), [self.visible_sprites, self.obstacle_sprites])
                 if column == 'p':
-                    Player((x, y), [self.visible_sprites])
+                    self.player = Player((x, y), [self.visible_sprites])
 
 
 
@@ -31,3 +32,5 @@ class Level:
     def run(self):
         #update and draw the game
         self.visible_sprites.draw(self.display_surface)
+        self.visible_sprites.update()
+        debug(self.player.direction)
