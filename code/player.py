3930fd8188ec9510c2c859ce8f3ea97999ada5bf
Biencredible <fabian.bien@gmx.de>
2023-02-06 07:55:53 +0100

    normalize diagonal movement

--- code/player.py
+++ code/player.py
@@ -8,6 +8,7 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect(topleft = pos)
 
         self.direction = pygame.math.Vector2()
+        self.speed = 5
 
     def input(self):
         keys = pygame.key.get_pressed()
@@ -25,6 +26,11 @@ class Player(pygame.sprite.Sprite):
         else:
             self.direction.x = 0
 
+    def move(self, speed):
+        if self.direction.magnitude() != 0:
+            self.direction = self.direction.normalize()
+        self.rect.center += self.direction * speed
+          
     def update(self):
         self.input()
-
+        self.move(self.speed)
