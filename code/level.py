11140f0e4161ee3ce924d778734544f5f6975c1a
Biencredible <fabian.bien@gmx.de>
2023-02-07 11:19:31 +0100

    Load map

--- code/level.py
+++ code/level.py
@@ -17,15 +17,15 @@ class Level:
         self.create_map()
 
     def create_map(self):
-        for row_index, row in enumerate(WORLD_MAP):
-            for column_index, column in enumerate(row):
-                x = column_index * TILESIZE
-                y = row_index * TILESIZE
-                if column == 'x':
-                    Tile((x, y), [self.visible_sprites, self.obstacle_sprites])
-                if column == 'p':
-                    self.player = Player((x, y), [self.visible_sprites], self.obstacle_sprites)
-
+        # for row_index, row in enumerate(WORLD_MAP):
+        #     for column_index, column in enumerate(row):
+        #         x = column_index * TILESIZE
+        #         y = row_index * TILESIZE
+        #         if column == 'x':
+        #             Tile((x, y), [self.visible_sprites, self.obstacle_sprites])
+        #         if column == 'p':
+        #             self.player = Player((x, y), [self.visible_sprites], self.obstacle_sprites)
+        self.player = Player((2000, 1430), [self.visible_sprites], self.obstacle_sprites)
 
 
 
@@ -44,10 +44,18 @@ class YSortCameraGroup(pygame.sprite.Group):
         self.half_height = self.display_surface.get_size()[1] // 2
         self.offset = pygame.math.Vector2()
 
+        #create the floor
+        self.floor_surface = pygame.image.load('../graphics/tilemap/ground.png').convert()
+        self.floor_rect = self.floor_surface.get_rect(topleft = (0, 0))
+
     def custom_draw(self, player):
         # getting the offset
         self.offset.x = player.rect.centerx - self.half_width
         self.offset.y = player.rect.centery - self.half_height
+
+        #draw floor
+        floor_offset_pos = self.floor_rect.topleft - self.offset
+        self.display_surface.blit(self.floor_surface, floor_offset_pos)
 
         for sprite in sorted(self.sprites(), key = lambda sprite: sprite.rect.centery):
             offset_pos = sprite.rect.topleft - self.offset
--- code/tile.py
+++ code/tile.py
@@ -2,8 +2,10 @@ import pygame
 from settings import *
 
 class Tile(pygame.sprite.Sprite):
-    def __init__(self, pos, groups):
+    def __init__(self, pos, groups, sprite_type, surface = pygame.surface((TILESIZE, TILESIZE))):
         super().__init__(groups)
-        self.image = pygame.image.load('../graphics/test/rock.png').convert_alpha()
+        self.sprite_type = sprite_type
+        self.image = surface
+        #self.image = pygame.image.load('../graphics/test/rock.png').convert_alpha()
         self.rect = self.image.get_rect(topleft = pos)
         self.hitbox = self.rect.inflate(0, -10)
